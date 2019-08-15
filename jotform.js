9340e9c90463121e7da0c2c14c1a4a1fd2286733
dakratochwill <39767176+dakratochwill@users.noreply.github.com>
2019-08-15 08:17:25 -0400

    Add files via upload

--- jotform.js
+++ jotform.js
@@ -10,50 +10,50 @@ JotForm.prepareCalculationsOnTheFly([null, null, {
     "qid": "2",
     "text": "Submit",
     "type": "control_button"
-            }, {
+}, {
     "description": "",
     "name": "name",
     "qid": "3",
     "text": "Name",
     "type": "control_fullname"
-            }, {
+}, {
     "description": "",
     "name": "email",
     "qid": "4",
     "subLabel": "example@example.com",
     "text": "Email",
     "type": "control_email"
-            }, {
+}, {
     "description": "",
     "name": "pleaseSelect",
     "qid": "5",
     "text": "Please select your areas of interest (check all that apply)",
     "type": "control_checkbox"
-            }]);
+}]);
 setTimeout(function () {
     JotForm.paymentExtrasOnTheFly([null, null, {
         "name": "submit2",
         "qid": "2",
         "text": "Submit",
         "type": "control_button"
-                }, {
+    }, {
         "description": "",
         "name": "name",
         "qid": "3",
         "text": "Name",
         "type": "control_fullname"
-                }, {
+    }, {
         "description": "",
         "name": "email",
         "qid": "4",
         "subLabel": "example@example.com",
         "text": "Email",
         "type": "control_email"
-                }, {
+    }, {
         "description": "",
         "name": "pleaseSelect",
         "qid": "5",
         "text": "Please select your areas of interest (check all that apply)",
         "type": "control_checkbox"
-                }]);
+    }]);
 }, 20);
