b79e6249e410fcea537f2d5055f003a0fbaad79d
dakratochwill <39767176+dakratochwill@users.noreply.github.com>
2019-08-22 09:49:07 -0400

    Add files via upload

--- jotform.js
+++ jotform.js
@@ -1,59 +1,89 @@
-JotForm.init(function () {
-    JotForm.alterTexts(undefined);
-    JotForm.clearFieldOnHide = "disable";
-    JotForm.submitError = "jumpToFirstError";
-    /*INIT-END*/
-});
+	JotForm.init(function () {
+	    JotForm.alterTexts(undefined);
+	    JotForm.clearFieldOnHide = "disable";
+	    JotForm.submitError = "jumpToFirstError";
+	    /*INIT-END*/
+	});
 
-JotForm.prepareCalculationsOnTheFly([null, null, {
-    "name": "submit2",
-    "qid": "2",
-    "text": "Submit",
-    "type": "control_button"
-}, {
-    "description": "",
-    "name": "name",
-    "qid": "3",
-    "text": "Name",
-    "type": "control_fullname"
-}, {
-    "description": "",
-    "name": "email",
-    "qid": "4",
-    "subLabel": "example@example.com",
-    "text": "Email",
-    "type": "control_email"
-}, {
-    "description": "",
-    "name": "pleaseSelect",
-    "qid": "5",
-    "text": "Please select your areas of interest (check all that apply)",
-    "type": "control_checkbox"
-}]);
-setTimeout(function () {
-    JotForm.paymentExtrasOnTheFly([null, null, {
-        "name": "submit2",
-        "qid": "2",
-        "text": "Submit",
-        "type": "control_button"
-    }, {
-        "description": "",
-        "name": "name",
-        "qid": "3",
-        "text": "Name",
-        "type": "control_fullname"
-    }, {
-        "description": "",
-        "name": "email",
-        "qid": "4",
-        "subLabel": "example@example.com",
-        "text": "Email",
-        "type": "control_email"
-    }, {
-        "description": "",
-        "name": "pleaseSelect",
-        "qid": "5",
-        "text": "Please select your areas of interest (check all that apply)",
-        "type": "control_checkbox"
-    }]);
-}, 20);
+	JotForm.prepareCalculationsOnTheFly([null, null, {
+	    "name": "submit2",
+	    "qid": "2",
+	    "text": "Submit",
+	    "type": "control_button"
+	}, {
+	    "description": "",
+	    "name": "name",
+	    "qid": "3",
+	    "text": "Name",
+	    "type": "control_fullname"
+	}, {
+	    "description": "",
+	    "name": "email",
+	    "qid": "4",
+	    "subLabel": "example@example.com",
+	    "text": "Email",
+	    "type": "control_email"
+	}, null, {
+	    "description": "",
+	    "name": "highSchool",
+	    "qid": "6",
+	    "subLabel": "",
+	    "text": "High School Graduation Year",
+	    "type": "control_textbox"
+	}, {
+	    "description": "",
+	    "name": "tellUs",
+	    "qid": "7",
+	    "subLabel": "",
+	    "text": "Tell us what you're interested in.",
+	    "type": "control_textarea"
+	}, {
+	    "description": "",
+	    "labelText": "",
+	    "name": "image",
+	    "qid": "8",
+	    "text": "Word%20Art%20(6).5d5e9bfdca9067.93419463",
+	    "type": "control_image"
+	}]);
+	setTimeout(function () {
+	    JotForm.paymentExtrasOnTheFly([null, null, {
+	        "name": "submit2",
+	        "qid": "2",
+	        "text": "Submit",
+	        "type": "control_button"
+	    }, {
+	        "description": "",
+	        "name": "name",
+	        "qid": "3",
+	        "text": "Name",
+	        "type": "control_fullname"
+	    }, {
+	        "description": "",
+	        "name": "email",
+	        "qid": "4",
+	        "subLabel": "example@example.com",
+	        "text": "Email",
+	        "type": "control_email"
+	    }, null, {
+	        "description": "",
+	        "name": "highSchool",
+	        "qid": "6",
+	        "subLabel": "",
+	        "text": "High School Graduation Year",
+	        "type": "control_textbox"
+	    }, {
+	        "description": "",
+	        "name": "tellUs",
+	        "qid": "7",
+	        "subLabel": "",
+	        "text": "Tell us what you're interested in.",
+	        "type": "control_textarea"
+	    }, {
+	        "description": "",
+	        "labelText": "",
+	        "name": "image",
+	        "qid": "8",
+	        "text": "Word%20Art%20(6).5d5e9bfdca9067.93419463",
+	        "type": "control_image"
+	    }]);
+	}, 20);
